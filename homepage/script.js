ac1baa5d7f32a1e5e1062ef7a2bd3d1ff4f7936b
Llylithe <81000091+Llylithe@users.noreply.github.com>
2025-03-07 22:13:33 +1100

    Add files via upload

---
+++ homepage/script.js
@@ -0,0 +1,69 @@
+window.onload = function () {
+  const canvas = document.getElementById('matrixCanvas');
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  matrix(canvas, {
+    chars: ['0', '1'],       // You can customize this for different characters
+    font_size: 20,            // Adjust the font size of the characters
+    color: '#070707'          // Set the matrix characters' color (green in this case)
+  });
+
+  let percentage = 0;
+  let period = 0;
+  const loadingText = document.getElementById("loading-text");
+  const titleText = document.getElementById("period");
+  const mainTitles = document.querySelectorAll('h1');
+  const description = document.getElementById("description");
+
+  const textToType = "An intense dissatisfaction with the state of the world, and a compulsion to do something about it.";
+  let textIndex = 0;
+
+  const typingSpeed = 30;
+  const loadingSpeed = 50;
+
+  function updateLoading() {
+    if (percentage < 100) {
+      percentage += 1;
+      loadingText.innerText = percentage + "%";
+      setTimeout(updateLoading, loadingSpeed);
+    } else {
+      setTimeout(() => {
+        document.body.classList.add("fade-out"); // Apply zoom-in fade effect
+        setTimeout(() => {
+          window.location.href = "homepage/home.html";
+        }, 1500); // Wait for animation to finish
+      }, 500);
+    }
+  }
+
+
+
+
+
+  function updateTitle() {
+    const dots = ["", ".", "..", "..."];
+    titleText.innerText = dots[period];
+    period = (period + 1) % 4;
+  }
+
+  function typeText() {
+    if (textIndex < textToType.length) {
+      description.innerHTML += textToType[textIndex];
+      textIndex++;
+      setTimeout(typeText, typingSpeed);
+    }
+  }
+
+  setTimeout(() => {
+    mainTitles.forEach(title => title.classList.add("visible"));
+    loadingText.classList.add("visible");
+  }, 100);
+
+  setTimeout(typeText, 350);
+  setTimeout(updateLoading, 200);
+  setInterval(updateTitle, 350);
+};
+
+
