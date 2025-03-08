13ac05f6b995b839142b49f59dfa7d9c4f20eac2
Llylithe <81000091+Llylithe@users.noreply.github.com>
2025-03-08 22:56:14 +1100

    asd;klfja;dslj

--- homepage/script.js
+++ homepage/script.js
@@ -9,6 +9,26 @@ window.onload = function () {
   matrix(canvas, {
     chars: ['0', '1'],       // You can customize this for different characters
     font_size: 20,            // Adjust the font size of the characters
-    color: '#111111'          // Set the matrix characters' color (green in this case)
+    color: '#050505'          // Set the matrix colour
   });
 };
+
+function showPopup() {
+  document.getElementById("keyPopup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("keyPopup").style.display = "none";
+}
+
+function checkKey() {
+  const inputKey = document.getElementById("keyInput").value;
+  const correctKey = "mySecretKey";
+
+  if (inputKey === correctKey) {
+    window.location.href = "home.html";
+  } else {
+    alert("Incorrect key. Please try again.");
+  }
+}
+
